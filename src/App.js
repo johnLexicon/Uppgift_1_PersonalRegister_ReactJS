900b0c52c8605fec98d516ce3442e6133ce52bd5
John Lundgren <john.lundgren.lexicon@gmail.com>
2020-07-18 16:51:02 +0200

    Added components and state to App component.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
+import EmployeeForm from "./components/EmployeeForm";
+import { Container } from "react-bootstrap";
+import EmployeeRegister from "./components/EmployeeRegister";
 
 function App() {
+  //useState returns an array [value, function]
+  const [employees, addEmployee] = useState([]);
+
+  const addNewEmployee = (name, salary) => {
+    const id = "234";
+    addEmployee([...employees].concat({ id: id, name: name, salary: salary }));
+  };
+
   return (
-    <div className="App">
-      <h3>The employee Register</h3>
-    </div>
+    <Container>
+      <h3 className="header">The employee Register</h3>
+      <EmployeeForm addNewEmployee={addNewEmployee} />
+      <EmployeeRegister employees={employees} />
+      {/* 
+      {employees.map((e) => (
+        <div>
+          <span>{e.name}</span>
+          <span>{e.salary}</span>
+        </div>
+      ))} */}
+    </Container>
   );
 }
 
